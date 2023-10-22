e6eee901311ae6bf111c9b0dfd83127a2de63818
74081akash <akashkakad8059@gmail.com>
2023-10-22 19:28:00 +0530

    Assignment_16

--- Assignment_16/Assignment_16_02/src/com/sunbeam/DbUtil.java
+++ Assignment_16/Assignment_16_02/src/com/sunbeam/DbUtil.java
@@ -1,27 +1,26 @@
 package com.sunbeam;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DbUtil {
+public class DbUtil
+{
+	public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-	private static String DB_DRIVER="com.mysql.cj.jdbc.Driver";
-	private static String DB_URL="jdbc:mysql://localhost:3306/practice";
-	private static String DB_USER="adnan";
-	private static String DB_PASSWORD="adnan8435";
-	
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/classwork";
+	public static final String DB_USER = "kd3_80109_AkashKakad";
+	public static final String DB_PASSWD = "manager";
 	static {
 		try {
 			Class.forName(DB_DRIVER);
-		} catch (Exception e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	
+
 	public static Connection getConnection() throws SQLException {
-		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
 		return con;
 	}
 }
